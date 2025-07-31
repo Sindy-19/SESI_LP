3da55e38c6edba3ed5e1be481b947c4710e25f45
sindy <sindy.lauren.19@gmail.com>
2025-07-31 11:29:31 -0300

    meu segundo commit

---
+++ comando_input.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void main()
+{ 
+    int a;
+    int b;
+
+    
+    printf("\n digite o valor de A:" );
+    scanf("%d", &a);
+     
+    printf("\n digite o valor de B:" );
+    scanf ("%d", &b);
+    int resultado = a-b;
+    
+    
+  
+  
+  printf("\n O VALOR É %d\n", resultado);
+  printf("%d - %d = %d ", a, b, resultado);
+}
