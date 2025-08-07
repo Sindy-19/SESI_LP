51e6d27c83b70147078a6f8f495100dfdba8758e
sindy <sindy.lauren.19@gmail.com>
2025-08-07 11:21:49 -0300

    entrada de dados

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+void main()
+{ 
+  int A = 1 ;
+  float B =2.1;
+  char C = 'S';
+  char NOME[5] = "SINDE";
+  
+  printf("\n valor de A %d", A);
+   printf("\n valor de B %2.f", B); 
+   printf("\n valor de C %c", C);
+   printf("\n valor de NOME %s", NOME);
+    
+    printf("\n valor de p1: %c", NOME [0]);
+     printf("\n valor de p1: %c", NOME [1]);
+     printf("\n valor de p1: %c", NOME [2]);
+     printf("\n valor de p1: %c", NOME [3]);
+     printf("\n valor de p1: %c", NOME [4]);
+    
+    
+    
+    
+}
