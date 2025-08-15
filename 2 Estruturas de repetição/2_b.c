0ae7c5fe16fffd0fd64a0f54b2244bee762b1faf
sindy <sindy.lauren.19@gmail.com>
2025-08-15 11:05:27 -0300

    atividades

---
+++ 2 Estruturas de repetição/2_b.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main (){
+    for(int i = 2; i <=50; i = i + 2){
+       if(i % 2==0);
+        printf("%d\n", i);
+       
+    }
+}
+
+
+
+
---
+++ 2 Estruturas de repetição/2_c.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void main(){
+    int i = 0;
+    int soma = 0;
+    
+    while(i <= 100){
+         soma = soma + i;
+        // printf("%d\n", i);
+        printf("\nPos: %d, Soma: %d", i, soma);
+        i++;
+    }
+    
+    
+    
+}
+
+
+
